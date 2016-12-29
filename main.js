1af0e86901ddc61dab74b7c649f42dafe707e11f
Nassir Isaf <njisaf@gmail.com>
2016-12-28 17:32:13 -0800

    main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const roleHarvester = require('./roles/harvester.js');
+const roleUpgrader = require('./roles/upgrader.js');
+const roleBuilder = require('./roles/builder.js');
+
+module.exports.loop = function () {
+
+    var tower = Game.getObjectById('TOWER_ID');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
