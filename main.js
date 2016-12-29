3a26207dcab79652edab63b9e8ec622421ecfae1
Nassir Isaf <njisaf@gmail.com>
2016-12-29 14:09:19 -0800

    Trying to move creeps off energy source

--- main.js
+++ main.js
@@ -15,35 +15,16 @@ module.exports.loop = function () {
     }
   }
 
-  // //spawn harvesters
-  //
-  // // var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  // // console.log('Harvesters: ' + harvesters.length);
-  // //
-  // // if(harvesters.length < 4) {
-  // //   var harvesterName = Game.spawns['The Base'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
-  // //   console.log('Spawning new harvester: ' + harvesterName);
-  // // }
-  //
-  // //spawn upgraders
-  //
-  // var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-  // console.log('Upgraders: ' + upgraders.length);
-  //
-  // if(upgraders.length < 2) {
-  //   var upgraderName = Game.spawns['The Base'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
-  //   console.log('Spawning new upgrader: ' + upgraderName);
-  // }
-  //
-  // //spawn builders
-  //
-  // var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-  // console.log('Builders: ' + builders.length);
-  //
-  // if(builders.length < 1) {
-  //   var builderName = Game.spawns['The Base'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
-  //   console.log('Spawning new upgrader: ' + builderName);
-  // }
+
+  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  console.log('Harvesters: ' + harvesters.length);
+
+  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  console.log('Upgraders: ' + upgraders.length);
+
+  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+  console.log('Builders: ' + builders.length);
+
 
   var tower = Game.getObjectById('TOWER_ID');
   if(tower) {
