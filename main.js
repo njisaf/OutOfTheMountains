6357cbc8287611c97a5c89efa5af464da4761b45
Nassir Isaf <njisaf@gmail.com>
2016-12-30 11:55:56 -0800

    trying to get this to work right

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var dataCount = require('data-creep-count');
-var helperSpawn = require('helper-spawn');
+// var dataCount = require('data-creep-count');
+// var helperSpawn = require('helper-spawn');
 var roleHarvester = require('role-harvester');
 var roleUpgrader = require('role-upgrader');
 var roleBuilder = require('role-builder');
@@ -15,15 +15,15 @@ module.exports.loop = function () {
     }
   }
 
-
-  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  console.log('Harvesters: ' + harvesters.length);
-
-  var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-  console.log('Upgraders: ' + upgraders.length);
-
-  var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-  console.log('Builders: ' + builders.length);
+  roleHarvester.spawn();
+  // var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+  // console.log('Harvesters: ' + harvesters.length);
+  //
+  // var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+  // console.log('Upgraders: ' + upgraders.length);
+  //
+  // var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+  // console.log('Builders: ' + builders.length);
 
 
   var tower = Game.getObjectById('TOWER_ID');
