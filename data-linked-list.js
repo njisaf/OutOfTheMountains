43b32cae0664dc64bd5bed5cc6a0c5d0abc7896a
Nassir Isaf <njisaf@gmail.com>
2016-12-31 09:58:21 -0800

    removeAt

--- data-linked-list.js
+++ data-linked-list.js
@@ -54,6 +54,30 @@ module.exports = function LinkedList() {
     return true;
   };
 
+  this.removeAt = function(position) {
+    if(position < 0 || position > length) {
+      console.log('Position is out of bounds;');
+      return false;
+    }
+
+    var current = head;
+    var previous = null;
+    var index = 0;
+
+    if(position === 0) {
+      console.log('Removing head. Next item in list will be assigned as head;');
+      head = current.next;
+    } else {
+      while(++index < position) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+    }
+    --length;
+    return current.element;
+  };
+
 
 
 };
