32be9febbb8e4974eef01acb9b6c89c55bdc443b
Nassir Isaf <njisaf@gmail.com>
2016-12-31 10:21:03 -0800

    Will they move?

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 'use strict';
 
+// const ll = require('data-linked-list');
+const GPU = require('role-gpu');
+
 module.exports.loop = function() {
 
   //clear dead creeps from memory
-
   for(var name in Memory.creeps) {
     if(!Game.creeps[name]) {
       delete Memory.creeps[name];
-      console.log('Clearing non-existing creep memory:', name);
+      console.log('Clearing non-existing creep memory: ', name);
     }
   }
 
-  //detect number of creeps in room
-    //loop over Memory.creeps, count roles, add to count
-      //is that necessary? perhaps we can pull directly from the Memory?
-  //detect creep roles in room
-
-  //detect structures in room
-  //detect state of structures in room?
-  //if structures in incomplete or damaged state, spawn GPU.
-  //ignore controller? controller gets upgraded if everything else done or if not at level.
-
-  //if all structures complete and controller upgraded, GPUs migrate? except for one? maintenance GPU;
-    //what we want eventually is for them to spawn some sort of migration convoy and then suicide;
+  //execute roles for all creeps
+  for(var creepName in Game.creeps) {
+    var creep = Game.creeps[creepName];
+    if(creep.memory.role === 'harvester') {
+      GPU.harvester(creep);
+    }
+    if(creep.memory.role === 'upgrader') {
+      GPU.upgrader(creep);
+    }
+    if(creep.memory.role === 'builder') {
+      GPU.builder(creep);
+    }
+  }
 
 };
