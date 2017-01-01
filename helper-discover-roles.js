959c3b8332ced3a6c38c5f5391739aefca15ae01
Nassir Isaf <njisaf@gmail.com>
2017-01-01 14:14:52 -0800

    assignRoles; somehow I think this will work

--- helper-discover-roles.js
+++ helper-discover-roles.js
@@ -10,8 +10,10 @@ module.exports = function() {
   //for now also let's do this only if we have full energy.
   for (var room in Memory.creepsByRoom) {
 
+    let gpuNameArray = Memory.creepsByRoom[room].gpuNameArray;
+
     //basic setup, get to five GPUs.
-    if(Memory.creepsByRoom[room].gpuNameArray.length < 5
+    if(gpuNameArray.length < 5
       && Game.rooms[room].energyAvailable === 300) {
         //we might be able to filter this according to available energy, but bugger for now;
       let spawn = Game.rooms[room].find(FIND_MY_SPAWNS);
@@ -20,11 +22,12 @@ module.exports = function() {
 
     //okay, that works. now, if there are five, and if energy is at capacity, switch to building;
     //switch by finding number of creeps, pushing that number of roles to an array, then we'll send it to the other helper;
-    if(Memory.creepsByRoom[room].gpuNameArray.length >= 5 && Game.rooms[room].energyAvailable === 300) {
-      let roleArray = new Array(Memory.creepsByRoom[room].gpuNameArray.length).fill('builder');
-      // roleArray.fill('builder');
-      console.log('roleArray: ', roleArray);
-      // assignRoles(room, roleArray);
+    if(gpuNameArray.length >= 5 && Game.rooms[room].energyAvailable === 300) {
+
+
+      let roleArray = new Array(gpuNameArray.length).fill('builder');
+      // console.log('roleArray: ', roleArray);
+      assignRoles(gpuNameArray, roleArray);
     }
 
   }
