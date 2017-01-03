7cc419178cb544f6bcf0c69c9867696bfcd1140a
Nassir Isaf <njisaf@gmail.com>
2017-01-03 13:15:13 -0800

    Right, I'm getting there; set room toggles next

--- helper-build-memory.js
+++ helper-build-memory.js
@@ -10,27 +10,52 @@ module.exports = {
   },
 
   scaffold: function() {
-    //scaffold checks to see if certain memory structures exist and creates them; for now, for bugfixing while we stumble forward;
+    //scaffold checks to see if certain memory structures exist and creates them; for now, this is for bugfixing while we stumble forward; pretty sure it's not necessary, the update functions should just create the objects as they go if they aren't there.
 
     if (!Memory.globalToggles) {
       Memory.globalToggles = {};
     }
 
-    if (!Memory.clausewitz) {
-      Memory.clausewitz = {};
+    if (!Memory.roomToggles) {
+      Memory.roomToggles = {};
     }
+
   },
 
   globalToggles: function() {
-    //globalToggles sets global targets, which I speculate should be:
-      //state of war or peace
-      //target rooms for reinforcement etc
+    //globalToggles sets global targets, basically anything that isn't a room level thing;
+    //For now, let's set some simple values
+
+    Memory.gpuSetupCount = 5;
 
   },
 
-  clausewitz: function() {
+  roomToggles: function() {
 
-    //clausewitz organizes info in the way we want it;
+    //these set conditional toggles for individual rooms, which subsequent functions act upon to dictate creep roles within the room;
+
+    //grab all friendly creeps in each friendly room.
+    for (var room in Game.rooms) {
+      let creepList = Game.rooms[room].find(FIND_MY_CREEPS);
+      let constructionSitesList = Game.rooms[room].find(FIND_CONSTRUCTION_SITES);
+
+      let newRoom = {
+        constructionSites: constructionSitesList,
+        gpuNameArray: [],
+        gpuRoles: {},
+      };
+
+      //loop over list, find GPUs, push to name array and role object;
+      for (var i = 0; i < creepList.length; ++i) {
+        let name = creepList[i].name;
+        if(Game.creeps[name].memory.model === 'GPU') {
+          newRoom.gpuNameArray.push(name);
+          newRoom.gpuRoles[name] = Game.creeps[name].memory.role;
+        }
+      }
+
+      Memory.creepsByRoom[room] = newRoom;
+    }
 
   },
 
--- helper-discover-roles.js
+++ helper-discover-roles.js
@@ -11,21 +11,21 @@ module.exports = function() {
     let gpuNameArray = Memory.creepsByRoom[room].gpuNameArray;
     let constructionSites = Memory.creepsByRoom[room].constructionSites;
     //basic setup, get to five GPUs.
-    if(gpuNameArray.length < 5
+    if(gpuNameArray.length < Memory.gpuSetupCount
       && Game.rooms[room].energyAvailable === 300) {
         //we might be able to filter this according to available energy, but bugger for now;
       let spawn = Game.rooms[room].find(FIND_MY_SPAWNS);
       spawnCreep(spawn[0].name, 'GPU', 'harvester', [WORK, CARRY, MOVE]);
     }
 
-    if (gpuNameArray.length >= 5 && Game.rooms[room].energyAvailable === 300 && constructionSites.length) {
+    if (gpuNameArray.length >= Memory.gpuSetupCount && Game.rooms[room].energyAvailable === 300 && constructionSites.length) {
 
       let roleArray = new Array(gpuNameArray.length).fill('builder');
       // console.log('roleArray: ', roleArray);
       assignRoles(gpuNameArray, roleArray);
     }
 
-    if (gpuNameArray.length >= 5 && Game.rooms[room].energyAvailable === 300 && !constructionSites.length) {
+    if (gpuNameArray.length >= Memory.gpuSetupCount && Game.rooms[room].energyAvailable === 300 && !constructionSites.length) {
 
       let roleArray = new Array(gpuNameArray.length).fill('upgrader');
       // console.log('roleArray: ', roleArray);
