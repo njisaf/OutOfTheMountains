a2c889d309ddf6b08e05fbdf087febb448a9b844
Nassir Isaf <njisaf@gmail.com>
2017-01-05 12:46:30 -0800

    main-clean-all.js

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 'use strict';
 
+const cleanAll = require('main-clean-all');
+
+// in loop, we put everything that needs to be done every tick. Simple as that. If we need it to do anything else we'll build another set of functions;
+
 module.exports.loop = function() {
 
   for (var room in Game.rooms) {
     //loop over rooms, determine what state they are in and set them to be in setup or whatever.
+
   }
 
   for (var creep in Game.creeps) {
     //loop over all creeps and execute their roles. Their roles should be literally the function name. It think I can do that with like .eval() right?
 
   }
+
+  cleanAll();
 
 };
