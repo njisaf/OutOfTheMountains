28e064bdcd3ad6ea20f81934840d14c7b365bc04
Nassir Isaf <njisaf@gmail.com>
2017-01-05 22:42:56 -0800

    This isn't right, not for the creeps. There def needs to be states and queues. I'll sleep on it

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 const executeRole = require('main-execute-role');
 const cleanAll = require('main-clean-all');
 
+const setupRatios = {harvester: 50, upgrader: 30, builder: 20};
+
 
 // in loop, we put everything that needs to be done every tick. Simple as that. If we need it to do anything else we'll build another set of functions;
 
@@ -16,12 +18,19 @@ module.exports.loop = function() {
     Memory.globalEnergyCapacityAvailable += room.energyCapacityAvailable;
 
     //i have no idea how to do this. let's just make it shit out spawns first.
-    //get count of all roles of creeps in room;
     let creepList = room.find(FIND_MY_CREEPS);
-    for (var creepName in creepList) {
-      let role = creepList[creepName].memory.role;
+    //get count of all roles of creeps in room;
+    for (var _creep in creepList) {
+      let role = creepList[_creep].memory.role;
       room.memory.creepRoleCount[role] += 1;
     }
+
+    //find the percentages of each role.
+    for (var role in room.memory.creepRoleCount) {
+
+    }
+
+    //this doesn't make sense at all. What I should be doing is setting queues or something.
   }
 
 
