c6463af0371de415bbb46f575f01eb8c5b206da4
Nassir Isaf <njisaf@gmail.com>
2017-01-05 17:26:35 -0800

    and again

--- main-get-room-energyavailable.js
+++ main-get-room-energyavailable.js
@@ -4,7 +4,7 @@
 
 module.exports = function(room) {
 
-  let energyAvailable = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     //no better way to do this, can't hold this in memory;
     let energyAvailable = room.energyAvailable;
 
@@ -17,7 +17,5 @@ module.exports = function(room) {
     }
 
   });
-
-  return energyAvailable;
 
 };
